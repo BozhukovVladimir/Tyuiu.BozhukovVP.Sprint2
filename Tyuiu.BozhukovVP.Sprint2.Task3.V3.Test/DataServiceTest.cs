5e9211210327827964fbd4536e0ec6430188dd36
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-10-16 18:22:07 +0500

    Task3

---
+++ Tyuiu.BozhukovVP.Sprint2.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,47 @@
+using Tyuiu.BozhukovVP.Sprint2.Task3.V3.Lib;
+namespace Tyuiu.BozhukovVP.Sprint2.Task3.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 18;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.167;
+            Assert.AreEqual(wait, res);
+
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -3;
+            double res = ds.Calculate(x);
+            double wait = 1;
+            Assert.AreEqual(wait, res);
+
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -10;
+            double res = ds.Calculate(x);
+            double wait = 0.1;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.BozhukovVP.Sprint2.Task3.V3/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.BozhukovVP.Sprint2.Task3.V3.Lib;
+namespace Tyuiu.BozhukovVP.Sprint2.Task3.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 Выполнил: Божуков В. П. ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Божуков В. П. ИБКСб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УCЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с использованием вложенных *");
+            Console.WriteLine("* оператор if-else, где пользователь вводит значение переменной X с клавиатуры. Округлить полученное *");
+            Console.WriteLine("* значение до трех знаков после запятой;                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите Х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Значение функции  = "+res);
+            Console.ReadLine();
+        }
+    }
+}
