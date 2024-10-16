74c6e034d06f0cfea12911f9f6d85205262f5b92
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-10-17 00:02:41 +0500

    Task6

---
+++ Tyuiu.BozhukovVP.Sprint2.Task6.V5/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BozhukovVP.Sprint2.Task6.V5.Lib;
+namespace Tyuiu.BozhukovVP.Sprint2.Task6.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 Выполнил: Божуков В. П. ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема:  Получение результата из switch                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Божуков В. П. ИБКСб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УCЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи оператора switch    *");
+            Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите номер карты: ");
+            int numCard = Convert.ToInt32(Console.ReadLine());
+            string res;
+            if ((numCard < 6) || (numCard > 14))
+            {
+                res = "Введено неверное значение";
+            }
+            else
+            {
+                res = "Эта карта: " + ds.FindCardValue(numCard);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
