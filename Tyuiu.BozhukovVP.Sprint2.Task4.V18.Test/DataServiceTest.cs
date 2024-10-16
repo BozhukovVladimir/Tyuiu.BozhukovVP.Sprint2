e7665c8b025d4c1db5fa9fa5bf0706b88641a7aa
Vladimir Bozhukov <bozhukovvladimir@yandex.ru>
2024-10-16 18:58:44 +0500

    Task4

---
+++ Tyuiu.BozhukovVP.Sprint2.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+using Tyuiu.BozhukovVP.Sprint2.Task4.V18.Lib;
+namespace Tyuiu.BozhukovVP.Sprint2.Task4.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = -2;
+            double y = -2;
+            double res = ds.Calculate(x, y);
+            double wait = 0.025;
+            Assert.AreEqual(res, wait);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 5;
+            double res = ds.Calculate(x, y);
+            double wait = 50;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.BozhukovVP.Sprint2.Task4.V18/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.BozhukovVP.Sprint2.Task4.V18.Lib;
+namespace Tyuiu.BozhukovVP.Sprint2.Task4.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 Выполнил: Божуков В. П. ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Божуков В. П. ИБКСб-24-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УCЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на с#, которая суммирует значение двух    *");
+            Console.WriteLine("* одинаоквых массивов по длине.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите Х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x, y);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Значение функции = " + res);
+            Console.ReadKey();
+        }
+    }
+}
